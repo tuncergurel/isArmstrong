f4f5ff836f71ffc9e88a157295a4728345dc5ad6
tuncergurel <you@example.com>
2020-05-01 22:01:26 +0300

    isArmstrong

---
+++ isArmstrong.py
@@ -0,0 +1,5 @@
+
+a = int(input("Number ?"))
+print("{} is armstrong".format(a)) if sum(list(map(lambda x, y = len(str(a)):x**y, list(map(int,str(a)))))) == a else print("{} isn't armstrong".format(a))
+
+
